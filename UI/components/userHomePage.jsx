7d05f41ba3ffe0aa9c065ab743e312d4b2cc5333
Int3gerunderflow <canidiansnowman@gmail.com>
2025-01-26 11:08:14 -0500

    The user homepage can now get every post associated with that user

--- UI/components/userHomePage.jsx
+++ UI/components/userHomePage.jsx
@@ -1,17 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import { useAuth } from './auth'
 
 function UserHomePage(){
+    const [userPosts, setUserPosts] = useState([])
     const {token} = useAuth();
     const payloadEncoded = token.split('.')[1]
     const payload = JSON.parse(atob(payloadEncoded))
-    console.log(payload)
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+          const response = await axios.get(`http://localhost:8000/users/posts/${payload.id}`);
+          setUserPosts(response.data)
+        };
+        fetchPosts();
+      }, []);
+
     return(
         <React.Fragment>
             <h2>hi</h2>
+            {userPosts.map((item)=> {
+                return <PostDescriptionCard key={item.postID} {...item}/>
+            })}
         </React.Fragment>
     )
 
+}
+
+const PostDescriptionCard = ({product, company})=>{
+    return(
+        <div style={{border: 2}}>
+            <h3>{product}</h3>
+            <h4>{company}</h4>
+        </div>
+    )
 }
 
 export default UserHomePage
