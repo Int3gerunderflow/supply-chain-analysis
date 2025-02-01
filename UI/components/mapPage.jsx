6190f1f8cb0dde5c6a3fe3c9ee539e2043bb4834
Int3gerunderflow <canidiansnowman@gmail.com>
2025-02-01 10:55:50 -0500

    Map now properly displays the relationships between suppliers

--- UI/components/mapPage.jsx
+++ UI/components/mapPage.jsx
@@ -1,7 +1,7 @@
 import React, {useState,useEffect} from 'react';
 import { getMapDataContext } from './mapData';
 import axios from 'axios';
-import {Map, NavigationControl, Popup, useControl} from 'react-map-gl/maplibre';
+import {Map as MapLibreMap, NavigationControl, Popup, useControl} from 'react-map-gl/maplibre';
 import {ScatterplotLayer, ArcLayer} from 'deck.gl';
 import {MapboxOverlay as DeckOverlay} from '@deck.gl/mapbox';
 import 'maplibre-gl/dist/maplibre-gl.css';
@@ -30,30 +30,59 @@ function DeckGLOverlay(props) {
 
 
 function MapPage() {
-  const {graphData,setadjList} = getMapDataContext()
+  const {graphData,setsuppData} = getMapDataContext()
   const [selected, setSelected] = useState(null);
+  const supplierHashMap = new Map();
 
   //create a new data object that holds every supplier in this user's post
   const [supplyData,setSupplyData] = useState([]) //list of every supplier
+  const [expandedAdjList, setExpAdjList] = useState([]) //array where every item contains a source and target destination
   const [finalAssemblyLocation, setFinalAssem] = useState(0)
 
-  const getAllSuppliers = async (adjList) => {
+  const getAllSuppliers = async (suppData) => {
     const suppliersArray = []
-    for(const element of adjList)
+    for(const element of suppData)
     {
       await axios.get(`http://localhost:8000/posts/supplier/${Number(Object.keys(element)[0])}`)
-         .then((reply)=>suppliersArray.push(reply.data))
+         .then((reply)=>{
+            suppliersArray.push(reply.data);
+            supplierHashMap.set(Number(Object.keys(element)[0]), reply.data)
+        })
     }
+    //once the hashmap is set also set up the expanded adjacency list
+    populateExpandedAdjList();
     return suppliersArray
+  }
+
+  const populateExpandedAdjList = () => {
+    const resultArray = []
+    for(const entry of graphData.adjacencyList)
+    {
+      const vertex = Number(Object.keys(entry)[0])
+      const neighbors = Object.values(entry)[0]
+      for(let n of neighbors)
+      {
+        const srcLongitude = supplierHashMap.get(vertex).longitude
+        const srcLatitude = supplierHashMap.get(vertex).latitude
+        const nLongitude = supplierHashMap.get(n).longitude
+        const nLatitude = supplierHashMap.get(n).latitude
+        resultArray.push({
+          "source": [srcLongitude,srcLatitude],
+          "target": [nLongitude, nLatitude]
+        })
+      }
+    }
+    console.log(resultArray)
+    setExpAdjList(resultArray)
   }
 
   useEffect(() => {
     const getSupplierInfo = async () => {
-      const adjList = await getAllSuppliers(graphData.adjacencyList);
+      const suppData = await getAllSuppliers(graphData.adjacencyList);
       //Go  through the adjacency list and find the supplier with the ID of the final assembly location
-      const finalAssem = adjList.filter((supplier)=>{return supplier.supplyID === graphData.finalAssembly})[0]
+      const finalAssem = suppData.filter((supplier)=>{return supplier.supplyID === graphData.finalAssembly})[0]
       setFinalAssem(finalAssem)
-      setSupplyData(adjList)
+      setSupplyData(suppData)
     };
     getSupplierInfo();
   }, []);
@@ -80,11 +109,11 @@ function MapPage() {
     }),
     new ArcLayer({
       id: 'arcs',
-      data: supplyData,
+      data: expandedAdjList,
       // dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
       // Styles
-      getSourcePosition: [finalAssemblyLocation.longitude,finalAssemblyLocation.latitude], // Wherever the final location is
-      getTargetPosition: f => [f.longitude,f.latitude],
+      getSourcePosition: f => [f.source[0], f.source[1]], // Wherever the final location is
+      getTargetPosition: f => [f.target[0], f.target[1]],
       getSourceColor: [0, 128, 200],
       getTargetColor: [200, 0, 80],
       getWidth: 1,
@@ -93,7 +122,7 @@ function MapPage() {
   ];
 
   return (
-    <Map initialViewState={INITIAL_VIEW_STATE} mapStyle={MAP_STYLE} dragRotate={false}>
+    <MapLibreMap initialViewState={INITIAL_VIEW_STATE} mapStyle={MAP_STYLE} dragRotate={false}>
       {selected && (
         <Popup
           key={selected.properties.name}
@@ -108,7 +137,7 @@ function MapPage() {
       )}
       <DeckGLOverlay layers={layers} /* interleaved*/ />
       <NavigationControl position="top-left" />
-    </Map>
+    </MapLibreMap>
   );
 }
 
